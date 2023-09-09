b4469bb385230c0515a8a296a3475922fd259ce1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-08 20:53:13 -0400

    feat: Add error.py to pygenesis/model
    - Introduce `mse`, `mse_prime`, and `regularized_mse` functions
    - These functions handle Mean Squared Error calculations
    
    This commit adds the `error.py` file to the `pygenesis/model` directory, providing functions for Mean Squared Error calculations.

---
+++ pygenesis/model/error.py
@@ -0,0 +1,18 @@
+"""
+pygenesis/model/error.py
+"""
+import numpy as np
+
+
+def mse(y_true, y_pred):
+    return np.mean(np.square(y_true - y_pred))
+
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / y_true.size
+
+
+def regularized_mse(y_true, y_pred, weights, lambda_):
+    mse_loss = np.mean(np.square(y_true - y_pred))
+    l2_penalty = lambda_ * np.sum(np.square(weights))
+    return mse_loss + l2_penalty
