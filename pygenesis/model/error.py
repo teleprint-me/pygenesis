475d5eb03ffaf5dac08c2c4187e03e5b4c74e171
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-08 21:21:27 -0400

    feat: Extend error.py in pygenesis/model
    - Add Cross-Entropy loss function for binary and multi-class classification
    - Implement Cross-Entropy derivative and regularization methods
    - Include comments to clarify regularization purpose
    
    This commit extends the `error.py` file in the `pygenesis/model` directory by introducing the Cross-Entropy loss function, its derivative, and regularization methods.

--- pygenesis/model/error.py
+++ pygenesis/model/error.py
@@ -27,6 +27,7 @@ class MSE(LossFunction):
 
     def regularized(self, y_true, y_pred, weights, lambda_=1e-5):
         """Regularized MSE using L2 regularization."""
+        # NOTE: 1e-5 is to prevent overfitting models
         mse_loss = np.mean(np.square(y_true - y_pred))
         l2_penalty = lambda_ * np.sum(np.square(weights))
         return mse_loss + l2_penalty
@@ -45,6 +46,30 @@ class RMS(LossFunction):
 
     def regularized(self, y_true, y_pred, weights, lambda_=1e-5):
         """Regularized RMS using L2 regularization."""
+        # NOTE: 1e-5 is to prevent overfitting models
         rms_loss = np.sqrt(np.mean(np.square(y_true - y_pred)))
         l2_penalty = lambda_ * np.sum(np.square(weights))
         return rms_loss + l2_penalty
+
+
+class CrossEntropy(LossFunction):
+    """Cross-Entropy loss function for binary and multi-class classification."""
+
+    def __call__(self, y_true, y_pred):
+        epsilon = 1e-15  # To prevent log(0)
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+    def prime(self, y_true, y_pred):
+        epsilon = 1e-15  # To prevent division by zero
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return -(y_true / y_pred) + (1 - y_true) / (1 - y_pred)
+
+    def regularized(self, y_true, y_pred, weights, lambda_=1e-5):
+        """Regularized Cross-Entropy using L2 regularization."""
+        # NOTE: 1e-5 is to prevent overfitting models
+        cross_entropy_loss = -np.mean(
+            y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)
+        )
+        l2_penalty = lambda_ * np.sum(np.square(weights))
+        return cross_entropy_loss + l2_penalty
