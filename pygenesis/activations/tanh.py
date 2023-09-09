ba7d0f9715150d7043ff46e518b8caa7ba40f355
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-09 14:40:06 -0400

    refactor: Migrate 'Tanh' Activation Class and Add Docstrings
    - Migrated the 'Tanh' activation class from 'pygenesis/model/activation.py' to 'pygenesis/activations/tanh.py'.
    - Added comprehensive docstrings to the 'Tanh' activation class for clarity and documentation.
    - Improved code organization for activation-related classes in 'pygenesis/activations' for enhanced maintainability.
    
    This commit optimizes code structure by migrating the 'Tanh' activation class and enhancing documentation, contributing to better code maintainability.

---
+++ pygenesis/activations/tanh.py
@@ -0,0 +1,56 @@
+"""
+pygenesis/activations/tanh.py
+
+This module defines the Tanh activation layer class for neural networks.
+"""
+import numpy as np
+
+from pygenesis.activations.base import Activation
+
+
+class Tanh(Activation):
+    """
+    Tanh activation layer for neural networks.
+
+    This class provides the Tanh activation function and its derivative.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Constructor method.
+        tanh(x): Compute the Tanh activation function.
+        tanh_prime(x): Compute the derivative of the Tanh activation function.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Tanh activation layer.
+        """
+        super().__init__(self.tanh, self.tanh_prime)
+
+    @staticmethod
+    def tanh(x):
+        """
+        Compute the Tanh activation function.
+
+        Args:
+            x (numpy.ndarray): The input data.
+
+        Returns:
+            numpy.ndarray: The result of applying the Tanh activation function to the input.
+        """
+        return np.tanh(x)
+
+    @staticmethod
+    def tanh_prime(x):
+        """
+        Compute the derivative of the Tanh activation function.
+
+        Args:
+            x (numpy.ndarray): The input data.
+
+        Returns:
+            numpy.ndarray: The derivative of the Tanh activation function applied to the input.
+        """
+        return 1.0 - np.tanh(x) ** 2
