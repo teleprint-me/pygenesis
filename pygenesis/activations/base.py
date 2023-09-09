d7d5ad49767066ff1f4b079815aa98c4bf7fecd2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-09 14:31:20 -0400

    refactor: Migrate 'Activation' Base Class and Add Docstrings
    - Migrated the 'Activation' base class from 'pygenesis/model/activation.py' to 'pygenesis/activations/base.py'.
    - Added comprehensive docstrings to the 'Activation' base class for clarity and documentation.
    - Streamlined code organization for activation-related classes in 'pygenesis/activations' for improved maintainability.
    
    This commit optimizes code structure by migrating the 'Activation' base class and enhancing documentation, contributing to better code maintainability.

---
+++ pygenesis/activations/base.py
@@ -0,0 +1,61 @@
+"""
+pygenesis/activations/base.py
+
+This module defines the base Activation layer class for neural networks.
+"""
+import numpy as np
+
+from pygenesis.model.layer import Layer
+
+
+class Activation(Layer):
+    """
+    Base class for activation layers in neural networks.
+
+    Attributes:
+        activation (function): The activation function used by the layer.
+        activation_prime (function): The derivative of the activation function.
+
+    Methods:
+        __init__(activation, activation_prime): Constructor method.
+        forward(input_data): Perform a forward pass through the layer.
+        backward(output_gradient, learning_rate): Perform a backward pass through the layer for gradient computation.
+    """
+
+    def __init__(self, activation, activation_prime):
+        """
+        Initializes the Activation layer.
+
+        Args:
+            activation (function): The activation function used by the layer.
+            activation_prime (function): The derivative of the activation function.
+        """
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input_data):
+        """
+        Perform a forward pass through the layer.
+
+        Args:
+            input_data (numpy.ndarray): The input data to the layer.
+
+        Returns:
+            numpy.ndarray: Output data after applying the activation function.
+        """
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        """
+        Perform a backward pass through the layer for gradient computation.
+
+        Args:
+            output_gradient (numpy.ndarray): The gradient of the loss with respect to the layer's output.
+            learning_rate (float): The learning rate for gradient descent.
+
+        Returns:
+            numpy.ndarray: The gradient of the loss with respect to the layer's input data.
+        """
+        return np.multiply(output_gradient, self.activation_prime(self.input))
