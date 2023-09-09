781b332d39502f1ef8280ab87dfbc1ba37d2e1f6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-08 20:54:40 -0400

    feat: Expand error.py in pygenesis/model
    - Add `rms`, `rms_prime`, and `regularized_rms` functions
    - These functions handle Root Mean Squared Error calculations
    
    This commit expands the `error.py` file in the `pygenesis/model` directory to include functions for Root Mean Squared Error calculations.

--- pygenesis/model/error.py
+++ pygenesis/model/error.py
@@ -16,3 +16,19 @@ def regularized_mse(y_true, y_pred, weights, lambda_):
     mse_loss = np.mean(np.square(y_true - y_pred))
     l2_penalty = lambda_ * np.sum(np.square(weights))
     return mse_loss + l2_penalty
+
+
+def rms(y_true, y_pred):
+    return np.sqrt(np.mean(np.square(y_true - y_pred)))
+
+
+def rms_prime(y_true, y_pred):
+    return (y_pred - y_true) / (
+        y_true.size * np.sqrt(np.mean(np.square(y_true - y_pred)))
+    )
+
+
+def regularized_rms(y_true, y_pred, weights, lambda_):
+    rms_loss = np.sqrt(np.mean(np.square(y_true - y_pred)))
+    l2_penalty = lambda_ * np.sum(np.square(weights))
+    return rms_loss + l2_penalty
