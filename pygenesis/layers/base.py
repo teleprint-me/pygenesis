ca95e727c52400acc9a8df744d34ebe38625ae58
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-09 13:50:51 -0400

    refactor: Move Layer Class to 'pygenesis/layers/base.py' and Add Docstrings
    - Moved the base Layer class from 'pygenesis/model/layer.py' to 'pygenesis/layers/base.py'.
    - Added docstrings to the Layer class following PEP-257 guidelines.
    - Improved code organization and documentation for enhanced clarity.
    
    This commit enhances code structure and documentation for the Layer class.

---
+++ pygenesis/layers/base.py
@@ -0,0 +1,53 @@
+"""
+pygenesis/layers/base.py
+
+This module defines the base layer class for neural network layers.
+"""
+from abc import ABC, abstractmethod
+
+
+class Layer(ABC):
+    """
+    Abstract base class for neural network layers.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Constructor method.
+        forward(input_data): Forward pass through the layer.
+        backward(output_gradient, learning_rate): Backward pass for gradient computation.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Layer instance.
+        """
+        ...
+
+    @abstractmethod
+    def forward(self, input_data):
+        """
+        Perform a forward pass through the layer.
+
+        Args:
+            input_data: The input data to the layer.
+
+        Returns:
+            Output data after applying the layer's transformation.
+        """
+        ...
+
+    @abstractmethod
+    def backward(self, output_gradient, learning_rate):
+        """
+        Perform a backward pass through the layer for gradient computation.
+
+        Args:
+            output_gradient: The gradient of the loss with respect to the layer's output.
+            learning_rate: The learning rate for gradient descent.
+
+        Returns:
+            The gradient of the loss with respect to the layer's input data.
+        """
+        ...
