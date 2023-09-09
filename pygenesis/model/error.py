8b38e9109966ad8ec15f0a7cc66581ba3f17a7e0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-08 21:11:46 -0400

    refactor: Refine error.py in pygenesis/model
    - Introduce a base class `LossFunction` for all loss functions
    - Implement the Mean Squared Error (MSE) loss function as a subclass
    - Implement the Root Mean Squared (RMS) loss function as a subclass
    - Add regularization methods for MSE and RMS
    - Improve code structure and maintainability
    
    This commit refines the `error.py` file in the `pygenesis/model` directory by introducing a base class for loss functions and organizing the code for MSE and RMS loss functions.

--- pygenesis/model/error.py
+++ pygenesis/model/error.py
@@ -4,31 +4,47 @@ pygenesis/model/error.py
 import numpy as np
 
 
-def mse(y_true, y_pred):
-    return np.mean(np.square(y_true - y_pred))
+class LossFunction:
+    """Base class for all loss functions."""
+
+    def __call__(self, y_true, y_pred):
+        """Calculates loss."""
+        pass
+
+    def prime(self, y_true, y_pred):
+        """Calculates the derivative of the loss."""
+        pass
 
 
-def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / y_true.size
+class MSE(LossFunction):
+    """Mean Squared Error loss function."""
+
+    def __call__(self, y_true, y_pred):
+        return np.mean(np.square(y_true - y_pred))
+
+    def prime(self, y_true, y_pred):
+        return 2 * (y_pred - y_true) / y_true.size
+
+    def regularized(self, y_true, y_pred, weights, lambda_=1e-5):
+        """Regularized MSE using L2 regularization."""
+        mse_loss = np.mean(np.square(y_true - y_pred))
+        l2_penalty = lambda_ * np.sum(np.square(weights))
+        return mse_loss + l2_penalty
 
 
-def regularized_mse(y_true, y_pred, weights, lambda_):
-    mse_loss = np.mean(np.square(y_true - y_pred))
-    l2_penalty = lambda_ * np.sum(np.square(weights))
-    return mse_loss + l2_penalty
+class RMS(LossFunction):
+    """Root Mean Squared loss function."""
 
+    def __call__(self, y_true, y_pred):
+        return np.sqrt(np.mean(np.square(y_true - y_pred)))
 
-def rms(y_true, y_pred):
-    return np.sqrt(np.mean(np.square(y_true - y_pred)))
+    def prime(self, y_true, y_pred):
+        return (y_pred - y_true) / (
+            y_true.size * np.sqrt(np.mean(np.square(y_true - y_pred)))
+        )
 
-
-def rms_prime(y_true, y_pred):
-    return (y_pred - y_true) / (
-        y_true.size * np.sqrt(np.mean(np.square(y_true - y_pred)))
-    )
-
-
-def regularized_rms(y_true, y_pred, weights, lambda_):
-    rms_loss = np.sqrt(np.mean(np.square(y_true - y_pred)))
-    l2_penalty = lambda_ * np.sum(np.square(weights))
-    return rms_loss + l2_penalty
+    def regularized(self, y_true, y_pred, weights, lambda_=1e-5):
+        """Regularized RMS using L2 regularization."""
+        rms_loss = np.sqrt(np.mean(np.square(y_true - y_pred)))
+        l2_penalty = lambda_ * np.sum(np.square(weights))
+        return rms_loss + l2_penalty
